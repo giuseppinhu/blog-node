2a742499356f605017852618f84832399e68afbb
Giuseppinhu <giuseppe.ribeiro.silva@gmail.com>
2025-10-14 13:12:29 -0300

    :construction: feat: More features  articles

--- articles/ArticleController.js
+++ articles/ArticleController.js
@@ -1,11 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const slugify = require('slugify');
 const Category = require('../categories/Category');
+const Article = require('../articles/Article');
+
+router.get('/admin/articles', (req, res) => {
+  Article.findAll({
+    include: [{ model: Category }],
+  }).then((articles) => {
+    res.render('admin/articles/index', { articles: articles });
+  });
+});
 
 router.get('/admin/articles/new', (req, res) => {
   Category.findAll().then((category) => {
     res.render('admin/articles/new', { categories: category });
   });
+});
+
+router.post('/article/save', (req, res) => {
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.create({
+    title: title,
+    body: body,
+    slug: slugify(title.toLowerCase()),
+    categoriaId: category,
+  }).then(() => {
+    res.redirect('/admin/articles');
+  });
+});
+
+router.post('/article/delete', (req, res) => {
+  var id = req.body.id;
+
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect('/admin/articles');
+      });
+    } else {
+      res.redirect('/admin/articles');
+    }
+  } else {
+    res.redirect('/admin/articles');
+  }
 });
 
 module.exports = router;
--- database/db.js
+++ database/db.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('blognode', 'root', '150725', {
   host: 'localhost',
   dialect: 'mysql',
+  timezone: '-03:00',
 });
 
 module.exports = connection;
