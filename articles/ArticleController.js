c58b40e7d28de9ceb2667d15e018b9a833a7d3cc
Giuseppinhu <giuseppe.ribeiro.silva@gmail.com>
2025-10-13 21:51:07 -0300

    :sparkles: feat: Article Form

--- articles/ArticleController.js
+++ articles/ArticleController.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
 
-router.get('/articles', (req, res) => {
-  res.send('Rota das categorias');
+router.get('/admin/articles/new', (req, res) => {
+  Category.findAll().then((category) => {
+    res.render('admin/articles/new', { categories: category });
+  });
 });
 
 module.exports = router;
